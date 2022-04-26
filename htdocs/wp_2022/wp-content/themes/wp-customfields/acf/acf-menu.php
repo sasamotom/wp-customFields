5cc710e5cc7bdd2a6d144465425d223214f7ffd3
sasamoto.m <sasamoto.m@nadia.bz>
2022-04-26 09:36:08 +0900

    [update] site3 TOPページで新商品を選択できるよう変更

--- htdocs/wp_2022/wp-content/themes/wp-customfields/acf/acf-menu.php
+++ htdocs/wp_2022/wp-content/themes/wp-customfields/acf/acf-menu.php
@@ -122,7 +122,6 @@ add_filter( 'term_link', 'rewrite_term_links_menu', 10, 3 );
 // カスタムフィールド定義（ACFのGenerate PHPをベースに作成）
 //----------------------------------------------
 if( function_exists('acf_add_local_field_group') ):
-
   acf_add_local_field_group(array(
     'key' => 'MENU_content',
     'title' => 'MENU',
@@ -211,50 +210,101 @@ if( function_exists('acf_add_local_field_group') ):
     'description' => '',
     'show_in_rest' => 0,
   ));
+endif;
 
+if( function_exists('acf_add_local_field_group') ):
+  acf_add_local_field_group(array(
+    'key' => 'newdonuts_content',
+    'title' => 'NEW DONUTS',
+    'fields' => array(
+      array(
+        'key' => 'newdonuts',
+        'label' => 'NEW DONUTSに表示するドーナツ',
+        'name' => 'newdonuts',
+        'type' => 'post_object',
+        'instructions' => 'TOPページに表示する新商品を１つ選択します。',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'post_type' => array(
+          0 => 'menu',
+        ),
+        'taxonomy' => '',
+        'allow_null' => 0,
+        'multiple' => 0,
+        'return_format' => 'id',
+        'ui' => 1,
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'page',
+          'operator' => '==',
+          'value' => '329',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+  ));
 endif;
 
 //----------------------------------------------
 // ショートコード
 //----------------------------------------------
-// // TOPページ用ニュース一覧（新しいものから３つ）
-// function getNewsListForTop($atts) {
-//   extract(shortcode_atts(array(
-//     "num" => '3',         // 取得記事数
-//     "cat" => 'news',       // 表示する記事のpost_type
-//     "tag" => '',
-//   ), $atts));
-//   global $post;
-//   $oldpost = $post;
-//   $myposts = get_posts('numberposts='.$num.'&order=desc&orderby=date&post_type='.$cat.'&tag='.$tag);
-//   if (!$myposts) {
-//     return false;
-//   }
-//   else {
-//     $retHtml='      <ul class="newsList">';
-//     foreach($myposts as $post) :
-//       $txt  = esc_html(get_the_title("","",false));
-//       setup_postdata($post);
-//       $retHtml.='<li><a href="'.esc_url(get_permalink()).'"><dl class="newsList_item">';
-//       $retHtml.='<dd class="newsList_date"><time datatime="'.get_the_date('Y-m-d', $post->ID).'">'.get_the_date('Y/n/j', $post->ID).'</time></dd>';
-//       $tax_slug = 'cat_news';         // タクソノミースラッグ指定
-//       $cat_terms = wp_get_object_terms($post->ID, $tax_slug); // タームの情報を取得
-//       if (!empty($cat_terms)) { // 変数が空でなければ true
-//         if (!is_wp_error($cat_terms)) {
-//           foreach ($cat_terms as $cat_term) {
-//             $retHtml.='<dd class="newsList_tag '.$cat_term->slug.'">'.$cat_term->name.'</dd>';
-//           }
-//         }
-//       }
-//       $retHtml.='<dt class="newsList_txt">'.$txt.'</dt>';
-//       $retHtml.='</dl></a></li>';
-//     endforeach;
-//     $retHtml.='</ul>';
-//     $post = $oldpost;
-//     wp_reset_postdata();
-//     return $retHtml;
-//   }
-// }
-// add_shortcode("newsListForTop", "getNewsListForTop"); // [newsListForTop]で呼び出せる
+// TOPページ用ニュース一覧（新しいものから３つ）
+function getNewDonutsForTop($atts) {
+  // get_field等が使えない場合は何もしない
+  if(!function_exists('get_field')) {
+    return;
+  }
+  $donuts_id = get_field('newdonuts');
+  if (!$donuts_id) {
+    return false;
+  }
+
+  global $post;
+  $oldpost = $post;
+  $mypost = get_post($donuts_id);
+  if (!$mypost) {
+    return false;
+  }
+  $post = $mypost;
+  $retHtml='<section class="topNew"><div class="container"><div class="twoCols">';
+  $imgPath = esc_url(get_field('image'));
+  $name  = esc_html(get_the_title("","",false));
+  $text  = get_field('text');
+  $price  = esc_html(get_field('price'));
+  setup_postdata($post);
+
+  $retHtml.='<div class="col"><h2 class="secTtl">NEW DONUTS</h2>';
+  if ($imgPath) {
+    $retHtml.='<div class="pic"><img src="'. $imgPath.'" alt="'.$name.'" '.get_image_size($imgPath).' loading="lazy" /></div>';
+  }
+  $retHtml.='</div>';
+  $retHtml.='<div class="col"><dl class="donutsInfo">';
+  $retHtml.='<dt class="donutsInfo_name">'. $name.'</dt>';
+  $retHtml.='<dd class="donutsInfo_txt">'.$text.'</dd>';
+  $retHtml.='<dd class="donutsInfo_price">¥'.$price.'<span class="-small">（税込）</span></dd>';
+  $retHtml.='</dl></div>';
+
+  $retHtml.='</div></div></section>';
+  $post = $oldpost;
+  wp_reset_postdata();
+  return $retHtml;
+}
+add_shortcode("newDonutsForTop", "getNewDonutsForTop"); // [newDonutsForTop]で呼び出せる
 
 ?>
